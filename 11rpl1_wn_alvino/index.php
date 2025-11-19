7136d2d4ad7a8dc564acc5c082ecfa47b1dd6cea
alvinoagung4-cmyk <alvinoagung4@gmail.com>
2025-11-19 08:27:35 +0700

    tahun ajaran 2025

--- 11rpl1_wn_alvino/index.php
+++ 11rpl1_wn_alvino/index.php
@@ -3,7 +3,7 @@
 <head>
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
-<title>WEBSISWA - Pendaftaran Siswa Baru</title>
+<title>WEBSISWA - Pendaftaran Siswa Baru 2025</title>
 </head>
 <body>
 <header>
